729c30c4db50df8ee822ae5fd3a30f231bd62434
adnanvanni <adnanvanni@gmail.com>
2022-05-11 16:35:37 +0530

    Minor fixes

--- Exercise/UnitTest1.cs
+++ Exercise/UnitTest1.cs
@@ -8,22 +8,28 @@ namespace Tests
 {
     public class Tests
     {
+        IWebDriver dr;
         [SetUp]
         public void Setup()
         {
-         
+            dr = new FirefoxDriver();
         }
 
         [Test]
-        public void Test1()
+        public void SearchComplicatedPageandOpen()
         {
-            IWebDriver dr = new FirefoxDriver();
+            ;
             POHome Home = new POHome(dr);
             Home.Open("https://ultimateqa.com/");
             Home.Search("complicated page");
             SearchResults SR = new SearchResults(dr);
             SR.findParticularLinkAndClickOnIt("Complicated Page");
             Assert.AreEqual(dr.Title, "Complicated Page | Ultimate QA");
+        }
+        [TearDown]
+        public void Close()
+        {
+            dr.Close();
         }
     }
 }
